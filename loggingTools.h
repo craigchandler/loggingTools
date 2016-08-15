d884945f52fb0fe5a0cc8156405cb003e94d2fb0
Craig Chandler <craig.chandler@interdynamics.com>
2016-08-15 16:31:28 +0930

    fix to work with several includes through project

--- loggingTools.h
+++ loggingTools.h
@@ -26,18 +26,18 @@ template <typename T>
 class Log
 {
 public:
-	Log();
-	virtual ~Log();
-	std::ostringstream& Get(TLogLevel level = logINFO);
+  Log();
+  virtual ~Log();
+  std::ostringstream& Get(TLogLevel level = logINFO);
 public:
-	static TLogLevel& ReportingLevel();
-	static std::string ToString(TLogLevel level);
-	static TLogLevel FromString(const std::string& level);
+  static TLogLevel& ReportingLevel();
+  static std::string ToString(TLogLevel level);
+  static TLogLevel FromString(const std::string& level);
 protected:
-	std::ostringstream os;
+  std::ostringstream os;
 private:
-	Log(const Log&);
-	Log& operator =(const Log&);
+  Log(const Log&);
+  Log& operator =(const Log&);
 };
 
 template <typename T>
@@ -48,80 +48,81 @@ Log<T>::Log()
 template <typename T>
 std::ostringstream& Log<T>::Get(TLogLevel level)
 {
-	os << "- " << NowTime();
-	os << " " << ToString(level) << ": ";
-	os << std::string(level > logDEBUG ? level - logDEBUG : 0, '\t');
-	return os;
+  os << "- " << NowTime();
+  os << " " << ToString(level) << ": ";
+  os << std::string(level > logDEBUG ? level - logDEBUG : 0, '\t');
+  return os;
 }
 
 template <typename T>
 Log<T>::~Log()
 {
-	os << std::endl;
-	T::Output(os.str());
+  os << std::endl;
+  T::Output(os.str());
 }
 
 template <typename T>
 TLogLevel& Log<T>::ReportingLevel()
 {
-	static TLogLevel reportingLevel = logNONE;
-	return reportingLevel;
+  static TLogLevel reportingLevel = logNONE;
+  return reportingLevel;
 }
 
 template <typename T>
 std::string Log<T>::ToString(TLogLevel level)
 {
-	static const char* const buffer[] = { "NONE", "ERROR", "WARNING", "INFO", "DEBUG", "DEBUG1", "DEBUG2", "DEBUG3", "DEBUG4" };
-	return buffer[level];
+  static const char* const buffer[] = { "NONE", "ERROR", "WARNING", "INFO", "DEBUG", "DEBUG1", "DEBUG2", "DEBUG3", "DEBUG4" };
+  return buffer[level];
 }
 
 template <typename T>
 TLogLevel Log<T>::FromString(const std::string& level)
 {
-	if (level == "DEBUG4")
-		return logDEBUG4;
-	if (level == "DEBUG3")
-		return logDEBUG3;
-	if (level == "DEBUG2")
-		return logDEBUG2;
-	if (level == "DEBUG1")
-		return logDEBUG1;
-	if (level == "DEBUG")
-		return logDEBUG;
-	if (level == "INFO")
-		return logINFO;
-	if (level == "WARNING")
-		return logWARNING;
-	if (level == "ERROR")
-		return logERROR;
-	Log<T>().Get(logWARNING) << "Unknown logging level '" << level << "'. Using INFO level as default.";
-	return logINFO;
+  if (level == "DEBUG4")
+    return logDEBUG4;
+  if (level == "DEBUG3")
+    return logDEBUG3;
+  if (level == "DEBUG2")
+    return logDEBUG2;
+  if (level == "DEBUG1")
+    return logDEBUG1;
+  if (level == "DEBUG")
+    return logDEBUG;
+  if (level == "INFO")
+    return logINFO;
+  if (level == "WARNING")
+    return logWARNING;
+  if (level == "ERROR")
+    return logERROR;
+  Log<T>().Get(logWARNING) << "Unknown logging level '" << level << "'. Using INFO level as default.";
+  return logINFO;
 }
 
 class Output2FILE
 {
 public:
-	static FILE*& Stream();
-	static void Output(const std::string& msg);
+  static FILE*& Stream();
+  static void Output(const std::string& msg);
 private:
-	static mutex _mtxFileOp;
+  static mutex& mtxFileOp() { static mutex mFO; return mFO; }
 };
+
 
 inline FILE*& Output2FILE::Stream()
 {
-	static FILE* pStream = stderr;
-	return pStream;
+  static FILE* pStream = stderr;
+  return pStream;
 }
 
 inline void Output2FILE::Output(const std::string& msg)
 {
-  _mtxFileOp.lock();
-	FILE* pStream = Stream();
-	if (!pStream)
-		return;
-	fprintf(pStream, "%s", msg.c_str());
-	fflush(pStream);
-  _mtxFileOp.unlock();
+  mtxFileOp().lock();
+  FILE* pStream = Stream();
+  if (!pStream)
+    return;
+  fprintf(pStream, "%s", msg.c_str());
+  fflush(pStream);
+  mtxFileOp().unlock();
 }
 
 #if defined(WIN32) || defined(_WIN32) || defined(__WIN32__)
@@ -154,16 +155,16 @@ class FILELOG_DECLSPEC FILELog : public Log<Output2FILE> {};
 
 inline std::string NowTime()
 {
-	const int MAX_LEN = 200;
-	char buffer[MAX_LEN];
-	if (GetTimeFormatA(LOCALE_USER_DEFAULT, 0, 0,
-		"HH':'mm':'ss", buffer, MAX_LEN) == 0)
-		return "Error in NowTime()";
+  const int MAX_LEN = 200;
+  char buffer[MAX_LEN];
+  if (GetTimeFormatA(LOCALE_USER_DEFAULT, 0, 0,
+    "HH':'mm':'ss", buffer, MAX_LEN) == 0)
+    return "Error in NowTime()";
 
-	char result[100] = { 0 };
-	static DWORD first = GetTickCount();
-	std::sprintf(result, "%s.%03ld", buffer, (long)(GetTickCount() - first) % 1000);
-	return result;
+  char result[100] = { 0 };
+  static DWORD first = GetTickCount();
+  std::sprintf(result, "%s.%03ld", buffer, (long)(GetTickCount() - first) % 1000);
+  return result;
 }
 
 #else
@@ -172,16 +173,16 @@ inline std::string NowTime()
 
 inline std::string NowTime()
 {
-	char buffer[11];
-	time_t t;
-	time(&t);
-	tm r = { 0 };
-	strftime(buffer, sizeof(buffer), "%X", localtime_r(&t, &r));
-	struct timeval tv;
-	gettimeofday(&tv, 0);
-	char result[100] = { 0 };
-	std::sprintf(result, "%s.%03ld", buffer, (long)tv.tv_usec / 1000);
-	return result;
+  char buffer[11];
+  time_t t;
+  time(&t);
+  tm r = { 0 };
+  strftime(buffer, sizeof(buffer), "%X", localtime_r(&t, &r));
+  struct timeval tv;
+  gettimeofday(&tv, 0);
+  char result[100] = { 0 };
+  std::sprintf(result, "%s.%03ld", buffer, (long)tv.tv_usec / 1000);
+  return result;
 }
 
 #endif //WIN32
